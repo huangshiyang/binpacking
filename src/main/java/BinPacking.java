791b32ef0b7dbc88bcc521a0aba6a88861389cb0
HUANG Shiyang <shiyang.huang@etu.unice.fr>
2018-01-02 21:29:40 +0100

    init

---
+++ src/main/java/BinPacking.java
@@ -0,0 +1,22 @@
+public class BinPacking {
+
+    static int nextFit(int[] weight, int capacity) {
+        return 0;
+    }
+
+    static int firstFit(int[] weight, int capacity) {
+        return 0;
+    }
+
+    static int bestFit(int[] weight, int capacity) {
+        return 0;
+    }
+
+    static int worstFit(int[] weight, int capacity) {
+        return 0;
+    }
+
+    static int almostWorstFit(int[] weight, int capacity) {
+        return 0;
+    }
+}
