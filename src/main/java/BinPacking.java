25ca7b7148b795e98fb1a4e257efc91fecca9dee
HUANG Shiyang <shiyang.huang@etu.unice.fr>
2018-01-02 22:49:02 +0100

    implement Next Fit

--- src/main/java/BinPacking.java
+++ src/main/java/BinPacking.java
@@ -1,22 +1,34 @@
+import java.util.List;
+
 public class BinPacking {
 
-    static int nextFit(int[] weight, int capacity) {
+    static int nextFit(List<Integer> weight, int capacity) {
+        int nBin = 0;
+        int binRemain = capacity;
+        for (int i = 0; i < weight.size(); i++) {
+            if (weight.get(i) > binRemain) {
+                nBin++;
+                binRemain = capacity - weight.get(i);
+            } else {
+                binRemain -= weight.get(i);
+            }
+        }
+        return nBin;
+    }
+
+    static int firstFit(List<Integer> weight, int capacity) {
         return 0;
     }
 
-    static int firstFit(int[] weight, int capacity) {
+    static int bestFit(List<Integer> weight, int capacity) {
         return 0;
     }
 
-    static int bestFit(int[] weight, int capacity) {
+    static int worstFit(List<Integer> weight, int capacity) {
         return 0;
     }
 
-    static int worstFit(int[] weight, int capacity) {
-        return 0;
-    }
-
-    static int almostWorstFit(int[] weight, int capacity) {
+    static int almostWorstFit(List<Integer> weight, int capacity) {
         return 0;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException {
+        List<String> lines = new ArrayList<>();
+        List<Integer> weight = new ArrayList<>();
+        int capacity = 0;
+        Files.lines(Paths.get("src/main/resources/monexemple.txt")).forEach(lines::add);
+        capacity = Integer.parseInt(lines.get(1));
+        String[] strings = lines.get(3).substring(0, lines.get(3).length() - 1).replace(" ", "").split(",");
+        for (int i = 0; i < strings.length; i++) {
+            weight.add(Integer.parseInt(strings[i]));
+        }
+        int nBin = BinPacking.nextFit(weight, capacity);
+        System.out.println(nBin);
     }
 }
